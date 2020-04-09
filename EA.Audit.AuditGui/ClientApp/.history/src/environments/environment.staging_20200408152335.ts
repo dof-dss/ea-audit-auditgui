684f5a50c78d88429224b63ea1d5198ab7e223fb
GrahamMcVea-dof <55088706+GrahamMcVea-dof@users.noreply.github.com>
2020-04-09 14:55:05 +0100

    Updates for staging environment

---
+++ EA.Audit.AuditGui/ClientApp/.history/src/environments/environment.staging_20200408152335.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    authUri: 'https://ea-staging.auth.eu-west-2.amazoncognito.com',
+    redirectUri: 'https://localhost:5001/',
+    clientId: '3j0feko5ai0b58d9k8ecdjupga',
+    apiUri: 'https://ea-audit-auditservice-staging.london.cloudapps.digital/api/v1/'
+    //apiUri: 'https://q3c58w7tu3.execute-api.eu-west-2.amazonaws.com/beta/api/v1/'
+  };
---
+++ EA.Audit.AuditGui/ClientApp/src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    authUri: 'https://ea-staging.auth.eu-west-2.amazoncognito.com',
+    redirectUri: 'https://ea-audit-auditgui-staging.london.cloudapps.digital/',
+    clientId: '4v00n260kmvdahh4voeqm53vpg',
+    apiUri: 'https://ea-audit-auditservice-staging.london.cloudapps.digital/api/v1/'
+  };
