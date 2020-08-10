94ba09ebba95a33a179565b02c00322522a36279
GrahamMcVea-dof <55088706+GrahamMcVea-dof@users.noreply.github.com>
2020-08-10 15:40:08 +0100

    Remove credentials

--- EA.Audit.AuditGui/ClientApp/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  authUri: 'https://ea-prod.auth.eu-west-2.amazoncognito.com',
-  redirectUri: 'https://ea-audit-auditgui.london.cloudapps.digital/',
-  clientId: '4qm8a133aj6ajvatq6fl9cfv53',
-  apiUri: 'https://audit.api.digitalni.gov.uk/v1/'
-};
--- EA.Audit.AuditGui/ClientApp/src/environments/environment.staging.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    authUri: 'https://ea-staging.auth.eu-west-2.amazoncognito.com',
-    redirectUri: 'https://ea-audit-auditgui-staging.london.cloudapps.digital/',
-    clientId: '5qd896gsoi3ndfshukh07t3s9o',
-    //apiUri: "https://930tfdy6z4.execute-api.eu-west-2.amazonaws.com/beta/v1/"
-    apiUri: 'https://audit-test.api.digitalni.gov.uk/v1/'
-  };
